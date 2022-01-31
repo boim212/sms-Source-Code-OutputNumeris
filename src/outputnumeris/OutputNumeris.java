7fe67b046599d6cdfa80e73db04b36ad94435a5c
boim212 <bimaboim7@gmail.com>
2022-01-31 19:21:46 +0700

    first commit

---
+++ src/outputnumeris/OutputNumeris.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package outputnumeris;
+
+/**
+ *
+ * @author Personal
+ */
+public class OutputNumeris {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*=================== OutputNumeris ===================*/
+/****************************************************
+* Program : OutputNumeris.java                      *
+* *                                                 *
+* Program ini untuk menunjukkan cara mengOUTPUTkan  *
+* bilangan numeris baik bulat maupun pecah          *
+****************************************************/
+        int a, b, c;
+        double z;
+        
+        //variabel
+        a = 2;
+        b = 3;
+        c = a + b;
+        z = 3.14;
+        
+        //print
+        System.out.println("================== Output Program ===================");
+        System.out.println("Nilai variabel a = " + a);
+        System.out.println("Nilai variabel b = " + b);
+        System.out.println("Nilai variabel c = a + b = " + c);
+        System.out.println("Nilai Variabel Pecahan z = " + z);
+    }
+}
